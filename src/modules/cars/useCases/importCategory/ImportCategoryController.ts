4103253c021e586b19a9df6cc1320fcd20014ad2
Matheus Amaral <matheus@matheusamaral.dev.br>
2022-07-16 10:42:03 -0300

    updates

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+class ImportCategoryController {
+  constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+    this.importCategoryUseCase.execute(file);
+    return response.send();
+  }
+}
+
+export { ImportCategoryController };
